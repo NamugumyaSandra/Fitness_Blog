50b93fcd6dfff79fe0181110829796ee293d3da3
Namugumya Sandra <salyn134@gmail.com>
2019-01-29 23:41:17 +0300

    Add login and register views to the aplication file.

--- fitness.py
+++ fitness.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template,url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET KEY'] = '9786f4c710cad7ed87cd4097403f376a'
 
 posts = [
     {
@@ -27,6 +30,17 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
    app.run(debug=True)
