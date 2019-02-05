3626306c19ddaf7db3711c2776a5f697ca007330
Namugumya Sandra <salyn134@gmail.com>
2019-02-05 11:33:10 +0300

    Add route to view specific user posts.

--- fitness/routes.py
+++ fitness/routes.py
@@ -11,7 +11,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route('/')
 @app.route('/home')
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page',1, type=int)
+    posts = Post.query.order_by(Post.date.desc()).paginate(page=page,per_page=5)
     return render_template('home.html',posts=posts)
 
 
@@ -122,3 +123,24 @@ def update_post(post_id):
         form.content.data = post.content
     return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
 
+
+@app.route('/post/<int:post_id>/delete',methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been successfully deleted!', 'success')
+    return redirect(url_for('home'))
+
+
+@app.route('/user/<string:username>')
+def user_posts(username):
+    page = request.args.get('page',1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date.desc())\
+        .paginate(page=page,per_page=5)
+    return render_template('user_posts.html',posts=posts, user=user)
